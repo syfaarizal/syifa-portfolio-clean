0141c81c7d1bcf93a612bbbeea6b1dd0e2e17119
syfaarizal <syifairgi@gmail.com>
2026-06-02 12:27:39 +0700

    refactor(about): rename svg variables and update icon sizes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,21 @@
-const projectIcon = () => (
-  <svg width="25" height="25" viewBox="0 0 48 48" fill="#7A0F16">
+const folderIcon = () => (
+  <svg width="23" height="23" viewBox="0 0 48 48" fill="#7A0F16">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier"><g id="Layer_2" data-name="Layer 2"> <g id="invisible_box" data-name="invisible box"> <rect width="48" height="48" fill="none"/> </g> <g id="icons_Q2" data-name="icons Q2"> <path d="M42,18V14a2,2,0,0,0-2-2H23L17,6H4A2,2,0,0,0,2,8V36a6,6,0,0,0,6,6H44a2,2,0,0,0,2-2V20a2,2,0,0,0-2-2ZM6,10h9.3l4.9,4.8L21.3,16H38v2H12a2,2,0,0,0-2,2V35.9A2.1,2.1,0,0,1,8.3,38,2,2,0,0,1,6,36V10Z"/> </g> </g></g>
   </svg>
 )
 
-const CalendarIcon = () => (
-  <svg width="25" height="25" viewBox="0 0 16 16" fill="none">
+const stackIcon = () => (
+  <svg version="1.1" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 32 32" fill="#7A0F16">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-    <g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM7 3V8.41421L10.2929 11.7071L11.7071 10.2929L9 7.58579V3H7Z" fill="#7A0F16"/> </g>
+    <g id="SVGRepo_iconCarrier"> <style type="text/css"> </style> <path d="M31,16v9c0,0.55-0.45,1-1,1H2c-0.55,0-1-0.45-1-1v-8c0-0.55,0.45-1,1-1h1c0.55,0,1,0.45,1,1 c0,0.55,0.45,1,1,1h6c0.55,0,1.201,0.402,1.447,0.894l0.106,0.211C12.799,19.597,13.45,20,14,20h4c0.55,0,1.201-0.403,1.447-0.894 l0.106-0.211C19.799,18.403,20.45,18,21,18h6c0.55,0,1-0.45,1-1l0-1c0-0.55,0.45-1,1-1h1C30.55,15,31,15.45,31,16z M6,17h20 c0.55,0,1-0.45,1-1s-0.45-1-1-1H6c-0.55,0-1,0.45-1,1S5.45,17,6,17z M6,14h20c0.55,0,1-0.45,1-1s-0.45-1-1-1H6c-0.55,0-1,0.45-1,1 S5.45,14,6,14z M6,11h20c0.55,0,1-0.45,1-1s-0.45-1-1-1H6c-0.55,0-1,0.45-1,1S5.45,11,6,11z M6,8h20c0.55,0,1-0.45,1-1s-0.45-1-1-1 H6C5.45,6,5,6.45,5,7S5.45,8,6,8z"/> </g>
   </svg>
 )
 
-const LocationIcon = () => (
-  <svg fill="#7A0F16" width="25" height="25" viewBox="0 -64 640 640">
+const laptopIcon = () => (
+  <svg fill="#7A0F16" width="23" height="23" viewBox="0 -64 640 640">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier">
@@ -24,8 +24,8 @@ const LocationIcon = () => (
   </svg>
 )
 
-const MailIcon = () => (
-  <svg width="25" height="25" viewBox="0 0 24 24" fill="none">
+const capIcon = () => (
+  <svg width="23" height="23" viewBox="0 0 24 24" fill="none">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5842 3.09053C11.8483 2.96982 12.1517 2.96982 12.4158 3.09053L22.4158 7.66195C22.7717 7.82467 23 8.18006 23 8.57143C23 8.96279 22.7717 9.31819 22.4158 9.4809L20 10.5853V16.4469C20 16.648 19.9495 16.8682 19.8221 17.0728C19.4914 17.6042 17.1726 21 12 21C6.82744 21 4.50856 17.6042 4.17785 17.0728C4.0505 16.8682 4 16.648 4 16.4469V10.5853L1.58424 9.4809C1.2283 9.31819 1 8.96279 1 8.57143C1 8.18006 1.2283 7.82467 1.58424 7.66195L11.5842 3.09053ZM6 11.4995V16.1994C6.48987 16.8789 8.31354 19 12 19C15.6865 19 17.5101 16.8789 18 16.1994V11.4995L12.4158 14.0523C12.1517 14.173 11.8483 14.173 11.5842 14.0523L6 11.4995ZM4.40524 8.57143L5.41576 9.03338L12 12.0433L18.5842 9.03338L19.5948 8.57143L12 5.09954L4.40524 8.57143ZM23.5 11.5C23.5 12.3284 22.8284 13 22 13C21.1716 13 20.5 12.3284 20.5 11.5C20.5 10.6716 21.1716 10 22 10C22.8284 10 23.5 10.6716 23.5 11.5ZM23 14.5C23 13.9477 22.5523 13.5 22 13.5C21.4477 13.5 21 13.9477 21 14.5V20C21 20.5523 21.4477 21 22 21C22.5523 21 23 20.5523 23 20V14.5Z" fill="#7A0F16"/> </g>
@@ -33,7 +33,7 @@ const MailIcon = () => (
 )
 
 const briefcaseIcon = () => (
-  <svg fill="#7A0F16" width="25" height="25" viewBox="0 0 32 32" version="1.1">
+  <svg fill="#7A0F16" width="23" height="23" viewBox="0 0 32 32" version="1.1">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier"> <path d="M30 5.984h-7.988v-1.938c0-1.655-1.346-3-3-3h-6.014c-1.655 0-3 1.345-3 3v1.938h-7.999c-1.099 0-2 0.901-2 2v7.008h-0.001v2h0.001v11.963c0 1.099 0.9 2 2 2h28c1.099 0 2-0.901 2-2v-20.971c0-1.099-0.901-2-2-2h0zM11.999 4.046c0-0.552 0.448-1 1-1h6.013c0.552 0 1 0.448 1 1v1.938h-8.014zM2.001 7.984h28v7.008h-11.012v-1.024c0-1.102-0.898-2-2-2h-1.992c-1.102 0-2 0.898-2 2v1.024h-10.996v-7.008zM16.99 19.004h-1.994v-5.036h1.992zM2 28.954v-11.963h10.996v2.012c0 1.102 0.897 2 2 2h1.992c1.102 0 2-0.898 2-2v-2.012h11.012v11.963h-28z"/> </g>
@@ -41,7 +41,7 @@ const briefcaseIcon = () => (
 )
 
 const cubesIcon = () => (
-  <svg fill="#7A0F16" height="25" width="25" version="1.1" viewBox="0 0 24 24">
+  <svg fill="#7A0F16" height="23" width="23" version="1.1" viewBox="0 0 24 24">
     <g id="SVGRepo_bgCarrier" stroke-width="0"/>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier"> <g id="cubes"> <path d="M17,23.1l-5-2.5l-5,2.5l-6-3v-7.2l5-2.8V3.9l6-3l6,3v6.5l5,2.5v7.2L17,23.1z M18,16.6v3.8l3-1.5v-3.8L18,16.6z M13,18.9 l3,1.5v-3.8l-3-1.5V18.9z M8,16.6v3.8l3-1.5v-3.8L8,16.6z M3,18.9l3,1.5v-3.8l-3-1.5V18.9z M14.2,13.5l2.8,1.4l2.8-1.4L17,12.1 L14.2,13.5z M4.1,13.4L7,14.9l2.9-1.4L7,11.8L4.1,13.4z M13,7.6v4.3l3-1.5V6.1L13,7.6z M8,10.1l3,1.7V7.6L8,6.1V10.1z M9.2,4.5 L12,5.9l2.8-1.4L12,3.1L9.2,4.5z"/> </g> </g>
@@ -50,22 +50,22 @@ const cubesIcon = () => (
 
 const infoCards = [
   {
-    icon: projectIcon,
+    icon: folderIcon,
     title: '10+',
     lines: ['Projects Built'],
   },
   {
-    icon: CalendarIcon,
+    icon: stackIcon,
     title: '4+',
     lines: ['Core Technologies'],
   },
   {
-    icon: LocationIcon,
+    icon: laptopIcon,
     title: 'Front-End',
     lines: ['Development'],
   },
   {
-    icon: MailIcon,
+    icon: capIcon,
     title: 'Modern Stack',
     lines: ['React & Next.js'],
   },
